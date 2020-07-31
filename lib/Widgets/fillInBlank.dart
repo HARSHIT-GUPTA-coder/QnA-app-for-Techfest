d3902b556b7132c8f5e225fa8bdac7329549043a
Harshit Gupta <mail4harshit1@gmail.com>
2020-07-31 16:24:00 +0530

    minor bug fix

--- lib/Widgets/fillInBlank.dart
+++ lib/Widgets/fillInBlank.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
-class FillInBlank extends StatelessWidget {
+class FillInBlank extends StatefulWidget {
   final handler;
+  final ans;
+  FillInBlank(this.handler, this.ans);
+
+  @override
+  _FillInBlankState createState() => _FillInBlankState();
+}
+
+class _FillInBlankState extends State<FillInBlank> {
   final _controller = TextEditingController();
-  FillInBlank(this.handler, var ans) {
-    if (ans is String)
-      this._controller.text = ans;
-    else
-      this._controller.text = '';
+
+  @override
+  void initState() {
+    _controller.text = widget.ans;
+    super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,13 +26,16 @@ class FillInBlank extends StatelessWidget {
         horizontal: 20,
       ),
       child: TextField(
-          controller: _controller,
-          decoration: InputDecoration(
-            hintText: 'Type your Answer here',
-          ),
-          onChanged: (String value) {
-            handler(this._controller.text);
-          }),
+        controller: _controller,
+        decoration: InputDecoration(
+          hintText: 'Type your Answer here',
+        ),
+        onEditingComplete: () {
+          print(_controller.text);
+          FocusScope.of(context).unfocus();
+          widget.handler(_controller.text);
+        },
+      ),
     );
   }
 }
